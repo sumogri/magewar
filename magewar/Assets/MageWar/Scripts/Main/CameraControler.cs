21e541cc25cccf3282175028c595d9c739b75b34
unknown <>
2016-10-15 18:10:04 +0900

    the first commit

---
+++ magewar/Assets/MageWar/Scripts/Main/CameraControler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControler : MonoBehaviour
+{
+
+    private GameObject target = null;
+    private Vector3[] offsets;
+    [SerializeField]
+    private Transform[] points;
+    private int pointsState = 0;
+    
+
+    void Start()
+    {
+        offsets = new Vector3[points.Length];
+        target = GameObject.FindGameObjectWithTag("Player");
+        //offsets = transform.position - target.transform.position;
+        for(int i = 0; i < points.Length; i++)
+        {
+            offsets[i] = points[i].position - target.transform.position;
+        }
+    }
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.x = target.transform.position.x + offsets[pointsState].x;
+        newPosition.y = target.transform.position.y + offsets[pointsState].y;
+        newPosition.z = target.transform.position.z + offsets[pointsState].z;
+        transform.position = Vector3.Lerp(transform.position, newPosition, 5.0f * Time.deltaTime);
+
+        bool button = Input.GetButtonDown("ZoomIn");
+        Debug.Log("zoomin " + button);
+    }
+}
---
+++ magewar/Assets/MageWar/Scripts/Main/CursorControler.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorControler : MonoBehaviour {
+    private float speed = 2f;
+    private float cursorMoveableTime = 0.1f;
+    private float asixInputingTime;
+    private bool inputable = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float hor = Input.GetAxis("Horizontal");
+        float var = Input.GetAxis("Vertical");
+        //Debug.Log("hor" + hor + "var" + var);
+
+        #region 一定時間おきに入力をとる
+        if (hor != 0f || var != 0f)
+        {
+            asixInputingTime += Time.deltaTime;
+            if(asixInputingTime >= cursorMoveableTime)
+            {
+                inputable = true;
+                asixInputingTime = 0;
+            }
+        }
+        else
+        {
+            asixInputingTime = 0;
+            inputable = true;
+        }
+        #endregion
+
+        if (inputable)
+        {
+            inputable = false;
+            if (hor > 0.5)
+            {
+                gameObject.transform.position += Vector3.right * speed;
+            }
+            else if (hor < -0.5)
+            {
+                gameObject.transform.position += Vector3.left * speed;
+            }
+            if (var > 0.5)
+            {
+                gameObject.transform.position += Vector3.forward * speed;
+            }
+            else if (var < -0.5)
+            {
+                gameObject.transform.position += Vector3.back * speed;
+            }
+        }
+    }
+}
